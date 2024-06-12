bf96af86cf14cf17be67841d0ebeababd4d46786
Jhonatan <jhonatanm255@hotmail.com>
2024-06-12 16:13:37 -0400

    modificacion para activar la camara trasera

--- qrExportDb.js
+++ qrExportDb.js
@@ -1,21 +1,25 @@
 
-    // Función para iniciar la cámara y leer códigos QR
-    
-
       // Función para iniciar la cámara y leer códigos QR
       const video = document.getElementById('video');
       const canvas = document.getElementById('canvas');
       const startButton = document.getElementById('startButton');
       const context = canvas.getContext('2d');
-
+      
       startButton.addEventListener('click', async () => {
         try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          const stream = await navigator.mediaDevices.getUserMedia({ 
+            video: { facingMode: 'environment' } // Acceder a la cámara trasera
+          });
           video.srcObject = stream;
-          video.style.display = "block";
-          canvas.style.display = "none";
-
-          video.addEventListener('autoPlay', () => {
+          video.play(); // Iniciar la reproducción del video automáticamente
+      
+          // Esperar a que se cargue el video
+          video.addEventListener('loadedmetadata', () => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+          });
+      
+          video.addEventListener('play', () => {
             console.log('Video is playing');
             setInterval(() => {
               context.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -30,15 +34,7 @@
           console.error('Error accessing the camera:', error);
         }
       });
-
-      // Additional logging to troubleshoot
-      video.addEventListener('loadedmetadata', () => {
-        console.log(`Video size: ${video.videoWidth}x${video.videoHeight}`);
-      });
-
-      video.addEventListener('error', (err) => {
-        console.error('Video element error:', err);
-      });
+      
 
 
     function exportarDatosCondominio(condominioId) {
