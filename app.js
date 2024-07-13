c8d955027a2dbdf6ca977384cf3ffb1b7e98aa04
Jhonatan <jhonatanm255@hotmail.com>
2024-07-12 23:12:37 -0500

    integre una validacion en la funcion de busqueda de propiedades

--- app.js
+++ app.js
@@ -131,11 +131,19 @@ function leerPropiedades() {
 }
 
 //FUNCION PARA BUSCAR PROPIEDADES
+
+
 function buscarPropiedad() {
   const nombre = document.getElementById('nombre').value.toLowerCase();
   const depto = document.getElementById('depto').value.toLowerCase();
   const estacionamiento = document.getElementById('est').value.toLowerCase();
   const bodega = document.getElementById('bodega').value.toLowerCase();
+
+  // Verificar si todos los campos están vacíos
+  if (nombre === '' && depto === '' && estacionamiento === '' && bodega === '') {
+  swal('Debe rellenar por lo menos un campo', '', 'warning');
+  return;
+  }
 
   const user = firebase.auth().currentUser;
   if (!user) {
